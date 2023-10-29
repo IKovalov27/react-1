0c542cf9dd67811bda313604111741b44e02a83a
Illia Kovalov <kovalev.illia2009@gmail.com>
2023-10-29 13:08:09 +0100

    171717

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
+const jacob = { 
+  id: `1`,
+  name: `Jacob Mercer`,
+  email: `j.mercer@mail.com`,
+  age: `32`,
+  avatar: `https://cdn-icons-png.flaticon.com/512/1077/1077012.png`,
+};
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+  return <div>
+    <div>
+      <p>
+        <b>Username:</b>
+        {jacob.name}
+      </p>
+      <p>
+        <b>Email:</b>
+        {jacob.email}
+      </p>
+      <p>Age</p>
+      <img src="avatar" alt="" />
     </div>
-  );
+  </div>
 };
