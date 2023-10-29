e3ea1f8a6c6f679a73c6d244e0f744566a321c4e
Illia Kovalov <kovalev.illia2009@gmail.com>
2023-10-29 17:47:28 +0100

    1111

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,14 @@ const jacob = {
   avatar: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXCSDHwbsJksZlqccC_Hdmaf75zl3VG0aTDnUQxS0uBbL9EjMOjejBe2fPQb9Gyh2rZ8w&usqp=CAU`,
 };
 
+const olendau = {
+  id: `3`,
+  name: `Olendau Jackson`,
+  email: `o.jackson@mail.com`,
+  age: `39`,
+  avatar: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0Vmtqupl5WKIsVe83rJSzJmWfIn-sdOq9P8Rk8iY7ymK6ZhgRrNv85sgYAjwHUsCiik0&usqp=CAU`,
+}
+
 export const App = () => {
   return <div>
     <div>
@@ -18,6 +26,19 @@ export const App = () => {
       </p>
       <p>
         <b>Age: </b> {jacob.age}
+      </p>
+    </div>
+
+    <div>
+      <img src={olendau.avatar} alt={olendau.name} width="120" height="120" />
+      <p>
+        <b>Username: </b> {olendau.name}
+      </p>
+      <p>
+        <b>Email: </b> {olendau.email}
+      </p>
+      <p>
+        <b>Age: </b> {olendau.age}
       </p>
     </div>
   </div>
