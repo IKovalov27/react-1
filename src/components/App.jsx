0725bf466964aa6f3a8423e94f8cbb525a8ca0ad
Illia Kovalov <kovalev.illia2009@gmail.com>
2023-10-31 17:10:26 +0100

    1111

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import { PilotList } from "./PilotList";
+import { FaBeer } from 'react-icons/fa';
 
 /* const jacob = { 
   id: `1`,
@@ -47,6 +48,7 @@ export const App = () => {
   return (
     <div>
       <h1>Top rated pilots</h1>
+      <FaBeer />
       <PilotList pilots={pilots} />
     </div>
   )
