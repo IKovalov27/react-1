c4298a75634080ab03055ad748c6cc1a95696321
Illia Kovalov <kovalev.illia2009@gmail.com>
2023-11-02 18:11:52 +0100

    171717117

--- src/components/PilotList/PilotList.js
+++ src/components/PilotList/PilotList.js
@@ -1,12 +1,12 @@
 import { Profile } from "../Profile/Profile";
-import { List } from "./PilotList.styled";
+import { List, ListItem } from "./PilotList.styled";
 
 export const PilotList = ({ pilots }) => {
     return (
         <ul style={List}>
             {pilots.map(pilot => {
                 return (
-                    <li key={pilot.id}>
+                    <li style={ListItem} key={pilot.id}>
                         <Profile 
                             imgUrl={pilot.avatar}
                             username={pilot.username}
--- src/components/PilotList/PilotList.styled.js
+++ src/components/PilotList/PilotList.styled.js
@@ -2,4 +2,11 @@ export const List = {
   padding: 0,
   margin: 0,
   display: "flex",
+  gap: 12,
+  listStyle: 'none',
+};
+
+export const ListItem = {
+  border: "1px solid black",
+  padding: 5,
 };
