3eddf4aaa2a423204efc94b44d76bdab423b9f52
Illia Kovalov <kovalev.illia2009@gmail.com>
2023-11-06 12:40:21 +0100

    272727 final

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -4,13 +4,13 @@ export const Profile = ({imgUrl, username, email, age}) => {
     return (
       <div>
         <img className={css.image} src={imgUrl} alt={username} width="120" height="120" />
-        <p class={css.alert}>
+        <p className={css.alert}>
           <b>Username: </b> {username}
         </p>
-        <p class={css.alert}>
+        <p className={css.alert}>
           <b>Email: </b> {email}
         </p>    
-        <p class={css.alert}>
+        <p className={css.alert}>
           <b>Age: </b> {age}
         </p>
       </div>
